947aca974c53403851964e314b5bcba5b8a9b2c3
Eskilll <eskil.nordlander@elev.ga.ntig.se>
2021-05-03 12:42:29 +0200

    initial

---
+++ GameCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
